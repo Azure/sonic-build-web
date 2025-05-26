c36a6aefb0383724d9af1054760e1b0a1d360a43
Liu Shilong <shilongliu@microsoft.com>
2025-05-26 21:23:35 +0800

    test

--- init.sh
+++ init.sh
@@ -5,77 +5,54 @@
 
 set -ex
 
-sudo sed -i 's/1/0/' /etc/apt/apt.conf.d/20auto-upgrades || true
+sed -i 's/1/0/' /etc/apt/apt.conf.d/20auto-upgrades || true
 source /etc/os-release
 
 function install_packages(){
     # install docker
-    sudo apt-get -o DPkg::Lock::Timeout=600 update
-    sudo apt-get -o DPkg::Lock::Timeout=600 install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg-agent \
-        software-properties-common || return
-
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-    add-apt-repository \
-       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-       $(lsb_release -cs) \
-       stable"
-
-    sudo apt-get -o DPkg::Lock::Timeout=600 update
-    sudo apt-get -o DPkg::Lock::Timeout=600 install -y docker-ce docker-ce-cli containerd.io || return
-
-    # install qemu for multi-arch docker
-    #sudo apt-get install -y qemu binfmt-support qemu-user-static
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" > /etc/apt/sources.list.d/docker.list
+    apt-get -o DPkg::Lock::Timeout=600 update
+    apt-get -o DPkg::Lock::Timeout=600 install -y docker-ce docker-ce-cli containerd.io
 
     # install utilities for image build
-    sudo apt-get -o DPkg::Lock::Timeout=600 install -y make || return
-    sudo apt-get -o DPkg::Lock::Timeout=600 install -y python3-pip || return
-    pip3 install --force-reinstall --upgrade jinja2==2.10 || return
-    pip3 install j2cli==0.3.10 markupsafe==2.0.1 || return
+    apt-get -o DPkg::Lock::Timeout=600 install -y make python3-pip
     # for team services agent
-    sudo apt-get -o DPkg::Lock::Timeout=600 install -y python-is-python2
+    # apt-get -o DPkg::Lock::Timeout=600 install -y python-is-python2
     # install python2 libvirt 5.10.0
-    sudo apt-get -o DPkg::Lock::Timeout=600 install -y python2-dev python-pkg-resources libvirt-dev pkg-config || return
-    curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2
-    pip2 install libvirt-python==5.10.0
-    pip2 install docker==4.4.1
+    # apt-get -o DPkg::Lock::Timeout=600 install -y python2-dev python-pkg-resources libvirt-dev pkg-config
+    # curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2
+    # pip2 install libvirt-python==5.10.0
+    # pip2 install docker==4.4.1
 
     # install packages for vs test
-    pip3 install pytest==4.6.2 attrs==19.1.0 exabgp==4.0.10 distro==1.5.0 docker==4.4.1 redis==3.3.4
-    sudo apt-get -o DPkg::Lock::Timeout=600 install -y libhiredis0.14 || return
+    pip3 install pytest exabgp docker redis
+    apt-get -o DPkg::Lock::Timeout=600 install -y libhiredis0.14
 
     # install packages for kvm test
-    sudo apt-get -o DPkg::Lock::Timeout=600 install -y libvirt-clients \
+    apt-get -o DPkg::Lock::Timeout=600 install -y libvirt-clients \
         qemu \
         openvswitch-switch \
         net-tools \
         bridge-utils \
-        util-linux \
-        iproute2 \
         vlan \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        software-properties-common \
         python3-libvirt \
         libzmq3-dev \
         libzmq5 \
-        libboost-serialization1.71.0 \
-        uuid-dev || return
+        libboost-serialization1.74.0 \
+        libboost1.74-dev \
+        libboost-dev \
+        libyang-dev \
+        uuid-dev
 }
 
-for ((i=0;i<10;i++));do
+for ((i=0;i<3;i++));do
     install_packages && break
-    sleep 30
+    sleep 3
 done
 
 # install br_netfilter kernel module
 modprobe br_netfilter
-
 # set sysctl bridge parameters for testbed
 sysctl -w net.bridge.bridge-nf-call-arptables=0
 sysctl -w net.bridge.bridge-nf-call-ip6tables=0
@@ -103,8 +80,7 @@ usermod -a -G sudo $tmpuser
 echo "$tmpuser ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/100-$tmpuser
 chmod 440 /etc/sudoers.d/100-$tmpuser
 
-sudo pip3 install docker==6.1.0 requests==2.31.0
-sudo apt-get -o DPkg::Lock::Timeout=600 install libyang0.16 libboost1.71-dev libboost-dev
+# pip3 install docker==6.1.0 requests==2.31.0
 
 # create two partition on the 1T data disk
 # first partition for azure pipeline agent
@@ -120,9 +96,3 @@ mkdir /agent
 mount /dev/${datadisk}1 /agent
 mkdir /data
 mount /dev/${datadisk}2 /data
-
-# clone sonic-mgmt repo
-pushd /data
-git clone https://github.com/Azure/sonic-mgmt
-chown -R $tmpuser.$tmpuser /data/sonic-mgmt
-popd
