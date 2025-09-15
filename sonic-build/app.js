cb9daf854b92ed390bddba31b9caf7c1ca7e7c05
Liu Shilong <shilongliu@microsoft.com>
2025-09-15 11:23:11 +0800

    fix

--- sonic-build/app.js
+++ sonic-build/app.js
@@ -16,7 +16,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger('combined'));
+app.set('trust proxy', true)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- sonic-build/routes/index.js
+++ sonic-build/routes/index.js
@@ -4,8 +4,7 @@ const url = require('url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.status(404).send('Not Found');
-  res.redirect('/ui/sonic/pipelines');
+  res.status(404).send('Not Found');
 });
 
 // UI entry
