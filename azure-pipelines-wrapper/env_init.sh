43a873a50b197bf0826f0911b5f557e2ef054f55
Liu Shilong <shilongliu@microsoft.com>
2026-03-17 23:24:15 +0800

    Install gh in GitHub App.

--- azure-pipelines-wrapper/env_init.sh
+++ azure-pipelines-wrapper/env_init.sh
@@ -3,6 +3,9 @@
 set -ex
 mkdir -p /data/workspace/daemon /workspace
 rm -rf /data/workspace/daemon/* -rf
+wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg
+chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt-get update
 apt-get install git jq gh parallel -y
 git config --global --add safe.directory '*'
